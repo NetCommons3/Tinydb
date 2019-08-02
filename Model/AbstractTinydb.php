a64baf2a41ed3c3efdb04cc52043d22b20d72785
Ryuji AMANO <ryuji@ryus.co.jp>
2019-08-03 06:07:45 +0900

    disable: AfterFrameSaveによるフレームの書換をとりやめ

--- Model/AbstractTinydb.php
+++ Model/AbstractTinydb.php
@@ -6,28 +6,28 @@ App::uses('TinydbAppModel', 'Tinydb.Model');
  */
 abstract class AbstractTinydb extends TinydbAppModel {
 
-/**
- * afterFrameSave
- *
- * @param array $frame Frame data
- * @return void
- */
-	public function afterFrameSave(array $frame) {
-		$this->__convertToTinydbFrame($frame);
-	}
-
-/**
- * フレームをTinydbのフレームにする
- *
- * @param array $frame Frame data
- * @return void
- */
-	private function __convertToTinydbFrame(array $frame) {
-		$frame['Frame']['plugin_key'] = 'tinydb';
-		$dbType = Inflector::underscore(get_class($this));
-		$frame['Frame']['default_setting_action'] = 'tinydb_blocks/index/db_type:' . $dbType;
-		$frameModel = ClassRegistry::init('Frames.Frame');
-		$frameModel->create();
-		$frameModel->save($frame, ['callbacks' => false]);
-	}
+///**
+// * afterFrameSave
+// *
+// * @param array $frame Frame data
+// * @return void
+// */
+//	public function afterFrameSave(array $frame) {
+//		$this->__convertToTinydbFrame($frame);
+//	}
+//
+///**
+// * フレームをTinydbのフレームにする
+// *
+// * @param array $frame Frame data
+// * @return void
+// */
+//	private function __convertToTinydbFrame(array $frame) {
+//		$frame['Frame']['plugin_key'] = 'tinydb';
+//		$dbType = Inflector::underscore(get_class($this));
+//		$frame['Frame']['default_setting_action'] = 'tinydb_blocks/index/db_type:' . $dbType;
+//		$frameModel = ClassRegistry::init('Frames.Frame');
+//		$frameModel->create();
+//		$frameModel->save($frame, ['callbacks' => false]);
+//	}
 }
