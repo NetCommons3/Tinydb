c7f8ddd995f2e9cba14cecf0cf2caa61f2462f7e
Ryuji AMANO <ryuji@ryus.co.jp>
2019-08-03 11:37:11 +0900

    change: TinydbのViewを使うときは各DBの具象プラグインのViewパスを優先で追加されるようにTinydbViewを変更

--- View/TinydbView.php
+++ View/TinydbView.php
@@ -4,6 +4,8 @@ App::uses('View', 'View');
 class TinydbView extends View {
 	protected function _paths($plugin = null, $cached = true) {
 		$paths = parent::_paths($plugin, $cached);
+
+		// TinydbのViewを使うときは具象プラグインのPathsを優先で追加
 		if ($plugin === 'Tinydb') {
 			$currentDbType = \Edumap\Tinydb\Lib\CurrentDbType::instance()->getDbType();
 			$currentDbTypePaths = parent::_paths($currentDbType);
@@ -15,6 +17,24 @@ class TinydbView extends View {
 			}
 			$this->_pathsForPlugin['Tinydb'] = $paths;
 			return $paths;
+		}
+
+		// Tinydb具象プラグインを使うときはTinydbのViewPathsを劣後で追加
+		if (\Edumap\Tinydb\Lib\CurrentDbType::instance()->getDbType() === $plugin) {
+			// TinydbのViewを劣後で追加
+			$tinydbPaths = parent::_paths('Tinydb');
+			$tinydbPaths = array_filter($tinydbPaths, function ($path) {
+				return strpos($path, 'Tinydb') !== false;
+			});
+
+			$currentPluginPaths = array_filter($paths, function ($path) {
+				return strpos($path, $this->plugin) !== false;
+			});
+			$otherPaths = array_filter($paths, function ($path) {
+				return strpos($path, $this->plugin) === false;
+			});
+			$paths = array_merge($currentPluginPaths, $tinydbPaths, $otherPaths);
+			$this->_pathsForPlugin[$this->plugin] = $paths;
 		}
 		return $paths;
 	}
